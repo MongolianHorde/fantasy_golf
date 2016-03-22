91cf1aa8faf99ec37c97efea2479e67e8ba04a32
dhughen93 <dhughen93@gmail.com>
2016-03-21 22:33:37 -0500

    Add full login and logout capabilities.

---
+++ login.php
@@ -0,0 +1,81 @@
+<?php
+	session_start();
+
+    require("constants.php");
+
+$id = $_POST['username']; // The id from login.html
+$password = $_POST['password'];
+
+// The database variable holds the connection so you can access it
+$database = mysqli_connect(DATABASEADDRESS,DATABASEUSER,DATABASEPASS);
+
+if (mysqli_connect_errno())
+{
+   echo "<h1>Connection error</h1>";
+}
+
+// Put your query here. Not required to put it in a variable but it's way cleaner and easier.
+// The question mark is for the bind_param below. Replace all variables with a question mark.
+$query = "SELECT first_name, last_name from manager WHERE manager_id = ? and password = ?";
+
+// The question mark is for the bind_param below. Replace all variables with a question mark.
+$adminQuery = "SELECT id, password from admin WHERE id = ? and password = ?";
+
+// The @ is for ignoring PHP errors. Replace "database_down()" with whatever you want to happen when an error happens.
+@ $database->select_db(DATABASENAME);
+
+// The statement variable holds your query      
+$stmt = $database->prepare($query);
+
+
+$adminStmt = $database->prepare($adminQuery);
+
+// Bind the result to PHP variables. The number of results MUST match the number specified in the query above. Names don't have to be the same.
+$stmt->bind_param("ss", $id, $password);
+$stmt->bind_result($stu1, $stu2);
+$adminStmt->bind_param("ss", $id, $password);
+$adminStmt->bind_result($admin1, $admin2);
+
+// ALWAYS use bind_param when you have a WHERE clause with $_GET and $_POST variables. This prevents SQL injection attacks.
+// Otherwise someone could put "drop table users" in the get url and drop your table. This prevents that.
+// First parameter is the number of parameters and their type. 1 s means 1 string. D means digit or decimal I believe.
+
+
+// Execute the SQL
+$stmt->execute();
+
+$type = ""; // Determines whether a student or teacher or nothing
+
+// Fetch all of the results one at a time. Access them from the variables in the bind_result
+
+while($stmt->fetch())
+{
+   echo $stu1 . " - " . $stu2 . " - ";
+}
+
+$adminStmt->execute();
+while($adminStmt->fetch())
+{
+   echo $admin1 . " - " . $admin2 . " - ";
+}
+
+// Post student id and go to student main page
+if($stu1 != null)
+{
+	$_SESSION['username'] = $id;
+	header('Location: managerHome.php');
+}
+// Post administrator id and go to administrator main page
+else if($admin1 != null)
+{
+	$_SESSION['username'] = $id;
+	header('Location: adminMainPage.php');
+}
+else
+{
+	 header('Location: login.html?errorMessage=Invalid username and password.');
+}
+
+// Close the database connection
+mysqli_close($database);
+?>
---
+++ logout.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE HTML>  
+<html>
+	<!-- Logout Page edited with timer and design by Victor Jereza -->
+<head> 
+    <meta name="author" content="Mongolian Horde" /> 
+	<meta http-equiv="Refresh" content="2.25; URL=login.html">
+ 	<link href="style.css" rel="stylesheet" type="text/css" /> 
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+	<link rel="shortcut icon" href="images/golfBallLogo.ico">
+
+
+ <title>FBBC Fantasy Golf</title> 
+	
+	<!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/logout.css" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+	
+</head> 
+<body> 
+<div class="centralBox">
+	<img class="centralLogo" src="images/golfBallLogo.jpg" alt="Our Logo"/>
+	<div class="test_republic_text">Fantasy Golf</div>
+	
+	<br /><br />
+		<h1 align="center" class="thx_text">Thank you for using FBBC fantasy golf!</h1>
+	
+	
+<div class="progress">
+		<div class="progress-bar progress-bar-striped active" role="progressbar"
+			aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width:100%; height:50px" > Redirecting to Login Page..
+		</div>
+	</div>
+</div>
+
+<?php
+	// Code to logout of the current page whether the user
+	// be student or teacher
+	// Author: David Hughen
+	// Date:   2/11/2015
+	session_start();
+	$_SESSION = array();
+	session_destroy();
+	
+	// TIMER CODE SHOULD GO HERE SOMEWHERE
+?>
+	    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+	
+</body> 
+</html> 
+
+
+
